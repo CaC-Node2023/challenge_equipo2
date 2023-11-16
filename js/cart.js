11034dd0f575e8826e3c2cac5ea1c1a1f1d1ec43
fedetrip <fedetripodicamara@gmail.com>
2023-11-16 17:32:13 -0300

    modificacion del feature para que muestre los valores en el resumen

--- js/cart.js
+++ js/cart.js
@@ -6,49 +6,75 @@ const decrementButton = document.getElementById("decrement_1");
 const incrementButton_2 = document.getElementById("increment_2");
 const decrementButton_2 = document.getElementById("decrement_2");
 
-const summaryQty = document.getElementById("summary__qty")
+const summaryQty = document.getElementById("summary__qty");
+const cartItem1QtyPrice = document.getElementById("cart-item1__qty-price");
+const cartItem2QtyPrice = document.getElementById("cart-item2__qty-price");
+const summarySubtotal = document.getElementById("summary__subtotal");
+const summaryTotal = document.getElementById("summary__total");
 
+const ItemPrice = 1799.99;
 
 incrementButton.addEventListener("click", function (event) {
   event.preventDefault();
-  incrementQuantity(qtyInput);
+  incrementQuantity(qtyInput, cartItem1QtyPrice);
   updateCart();
 });
 
 decrementButton.addEventListener("click", function (event) {
   event.preventDefault();
-  decrementQuantity(qtyInput);
+  decrementQuantity(qtyInput, cartItem1QtyPrice);
   updateCart();
 });
 
 incrementButton_2.addEventListener("click", function (event) {
   event.preventDefault();
-  incrementQuantity(qtyInput_2);
+  incrementQuantity(qtyInput_2, cartItem2QtyPrice);
   updateCart();
 });
 
 decrementButton_2.addEventListener("click", function (event) {
   event.preventDefault();
-  decrementQuantity(qtyInput_2);
+  decrementQuantity(qtyInput_2, cartItem2QtyPrice);
   updateCart();
 });
 
-function incrementQuantity(inputElement) {
+function incrementQuantity(inputElement, qtyPriceElement) {
   const currentValue = parseInt(inputElement.value, 10);
   inputElement.value = currentValue + 1;
+  updateQtyPrice(inputElement, qtyPriceElement);
 }
 
-function decrementQuantity(inputElement) {
+function decrementQuantity(inputElement, qtyPriceElement) {
   const currentValue = parseInt(inputElement.value, 10);
   if (currentValue > 1) {
     inputElement.value = currentValue - 1;
+    updateQtyPrice(inputElement, qtyPriceElement);
   }
 }
 
+function updateQtyPrice(inputElement, qtyPriceElement) {
+  const qtyValue = parseInt(inputElement.value, 10);
+  const newQtyPrice = qtyValue * ItemPrice;
+  qtyPriceElement.textContent = `$ ${newQtyPrice.toFixed(2)}`;
+}
+
 function updateCart() {
-    const qtyValue1 = parseInt(qtyInput.value, 10);
-    const qtyValue2 = parseInt(qtyInput_2.value, 10);
-    const totalQty = qtyValue1 + qtyValue2;
+  const qtyValue1 = parseInt(qtyInput.value, 10);
+  const qtyValue2 = parseInt(qtyInput_2.value, 10);
+  const subtotal1 = qtyValue1 * ItemPrice;
+  const subtotal2 = qtyValue2 * ItemPrice;
+  const totalQty = qtyValue1 + qtyValue2;
+  const totalSubtotal = subtotal1 + subtotal2;
+
+  if (summaryQty) {
     summaryQty.textContent = totalQty;
   }
-  
+
+  if (summarySubtotal) {
+    summarySubtotal.textContent = `$ ${totalSubtotal.toFixed(2)}`;
+  }
+
+  if (summaryTotal) {
+    summaryTotal.textContent = `$ ${totalSubtotal.toFixed(2)}`;
+  }
+}
