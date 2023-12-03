2e657104a52c0b561de13ffa2ef9b180b82cba3b
Naninho Carioca <cgferrarotti@gmail.com>
2023-12-03 20:23:30 -0300

    fix(node): corrijo método en las rutas definidas en la mission 4

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express();
+const router = express.Router();
 
 const shopControllers = require('../controllers/shopControllers');
 
@@ -8,6 +8,5 @@ router.get('/item/:id', shopControllers.item);
 router.post('/item/:id/add', shopControllers.addItemToCart);
 router.get('/cart', shopControllers.cart);
 router.post('/cart', shopControllers.checkout);
-
 
 module.exports = router;
