79ab8b951b859b811e1606e1c9b16157b99326cf
Naninho Carioca <cgferrarotti@gmail.com>
2023-12-03 21:45:31 -0300

    fix(node): acomodamos rutas de la página principal para que apunten a las vistas

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -2,6 +2,9 @@ const mainControllers = {
     home: (req, res) => {
         res.render('home', {
             title: 'Funkoshop | Inicio',
+            item1: 1,
+            item2: 2,
+            item3: 3
         })
     },
     about:(req, res) => {
