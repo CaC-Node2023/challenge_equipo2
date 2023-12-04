239ae2b82d7f478b0013ffa12e56aeeafb4bcdc2
fedetrip <fedetripodicamara@gmail.com>
2023-12-04 17:24:52 -0300

    feat shop con cards dinamicas

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path')
+
 const shopControllers = {
     shop: (req, res) => {
+        const items = fs.readFileSync(path.resolve(__dirname, '../data/items.json'));
+        const productos = JSON.parse(items);
         res.render('shop/shop', {
             title: 'Funkoshop | Tienda',
+            productos
         })
     },
     item: (req, res) => {
@@ -12,7 +18,7 @@ const shopControllers = {
     addItemToCart: (req, res) => {
         res.send('Esta ruta agrega un ítem al carrito.')
     },
-    cart: (req, res) => {
+    cart: (req, res) => { 
         res.render('shop/cart', {
             title: 'Funkoshop | Carrito',
         })
