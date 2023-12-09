0ea844f81d59fae1d1634a4c47b985679771cada
Naninho Carioca <cgferrarotti@gmail.com>
2023-12-09 03:58:24 -0300

    feat(ejs): colecciones dinámicas para home.ejs

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,15 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
 const mainControllers = {
     home: (req, res) => {
+        const home = fs.readFileSync(path.resolve(__dirname, '../data/home.json'));
+        const licences = JSON.parse(home).licences;
+
         res.render('home', {
             title: 'Funkoshop | Inicio',
-            item1: 1,
-            item2: 2,
-            item3: 3
+            licences
         })
     },
+
     about:(req, res) => {
         res.send('Esta ruta devuelve la página "Acerca de".')
     },
+
     faqs: (req, res) => {
         res.send('Esta ruta devuelve la página "Preguntas frecuentes".')
     }
