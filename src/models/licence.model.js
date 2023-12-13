75e63df4e6aaf379d6dc03e97c8a51f86eb51ef2
fedetrip <fedetripodicamara@gmail.com>
2023-12-13 16:28:28 -0300

    feat(db): creacion del licence.model.js

---
+++ src/models/licence.model.js
@@ -0,0 +1,19 @@
+const { conn } = require('../config/conn');
+
+const getLicences = async connection => {
+    try {
+        const [ rows ] = await conn.query('SELECT * FROM product;');
+        return rows;
+    } catch (error) {
+        return {
+            error: true,
+            message: 'Hemos encontrado un error ' + error
+        }
+    } finally {
+        conn.releaseConnection() 
+    }
+};
+
+module.exports = {
+    getLicences
+};
