1079671b46ec7c89998131dc69ab72f5b7a47973
fedetrip <fedetripodicamara@gmail.com>
2023-12-13 16:29:23 -0300

    feat(db) implemento la lectura de los productos deade la base de datos para la pagina shop

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,7 @@ const { conn } = require('../config/conn');
 
 const getProducts = async () => {
     try {
-        const [ rows ] = await conn.query('SELECT * FROM product;');
+        const [ rows ] = await conn.query('SELECT product.*, licence.licence_name FROM product JOIN licence ON product.licence_id = licence.licence_id;');
         return rows;
     } catch (error) {
         return {
