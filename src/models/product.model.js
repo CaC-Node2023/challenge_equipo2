433469bec430b1231ee14d3848f26d2d557f2c6d
naninho205 <cgferrarotti@gmail.com>
2023-12-14 01:30:48 -0300

    feat(controllers,models,views): agregamos la lógica para insertar nuevos productos; cambiamos temporalmente el enctype en el ejs (hasta implementar multer)

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,16 @@ const { conn } = require('../config/conn');
 
 const getProducts = async () => {
     try {
-        const [ rows ] = await conn.query('SELECT product.*, licence.licence_name FROM product JOIN licence ON product.licence_id = licence.licence_id;');
+        const [ rows ] = await conn.query(  'SELECT' +
+                                                'product.*,' +
+                                                'licence.licence_name' +
+                                                'category.category_name' +
+                                            'FROM product' +
+                                            'INNER JOIN licence' +
+                                                'ON product.licence_id = licence.licence_id' +
+                                            'INNER JOIN category' +
+                                                'ON product.category_id = category.category_id' +
+                                            ';');
         return rows;
     } catch (error) {
         return {
@@ -14,6 +23,33 @@ const getProducts = async () => {
     }
 };
 
+const addProduct = async (request) => {
+    try {
+        const [ rows ] = await conn.query(  'INSERT INTO product' +
+                                                '(product_name,' +
+                                                'product_description,' +
+                                                'product_price,' +
+                                                'stock,' +
+                                                'discount,' +
+                                                'product_sku,' +
+                                                'dues,' +
+                                                'img_front,' +
+                                                'img_back,' +
+                                                'category_id,' +
+                                                'licence_id)' +
+                                            'VALUES ?;', [request]);
+        return rows;
+    } catch (error) {
+        return {
+            error: true,
+            message: 'Hemos encontrado un error ' + error
+        }
+    } finally {
+        conn.releaseConnection()
+    }
+};
+
 module.exports = {
-    getProducts
+    getProducts,
+    addProduct
 };
